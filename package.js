5c2f5835918d8446c5f007b698e401dc2b305850
Ruslan Shashkov <rshashkov@icloud.com>
2016-03-25 12:51:12 +0200

    0.0.2

--- package.js
+++ package.js
@@ -1,13 +1,10 @@
 // package metadata file for Meteor.js
 var packageName = 'rshashkov:angular-meteor-datepicker-moment';
 var where = 'client'; // where to install: 'client' or 'server'. For both, pass nothing.
-var version = '0.0.1';
+var version = '0.0.2';
 var summary = 'AngularJS datepicker directives';
 var gitLink = 'https://github.com/ruslanchek/angular-meteor-datepicker-moment';
 var documentationFile = 'README.md';
-var packages = {
-	"momentjs:moment": {}
-};
 
 // Meta-data
 Package.describe({
@@ -15,11 +12,11 @@ Package.describe({
     version: version,
     summary: summary,
     git: gitLink,
-	packages: packages,
     documentation: documentationFile
 });
 
 Package.onUse(function(api) {
     api.versionsFrom(['METEOR@0.9.0', 'METEOR@1.0']); // Meteor versions
+	api.use(['momentjs:moment'], 'client');
     api.addFiles('src/datepicker-moment.js', where); // Files in use
 });
